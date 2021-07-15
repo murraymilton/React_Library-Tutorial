4239180c6e3b92ff2bf6af554ffa6b1341347a16
mmilton <murray.l.milton@outlook.com>
2021-07-15 15:27:26 +0200

    Initial commit for React_Tutorial completed. Pending review, by dcc.

---
+++ src/components/TitleBar/titleBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './titleBar.css';
+
+
+function TitleBar(props) {
+    return (
+        <div className="row row-spacer">
+            <div className="col-md-12" style={{padding: 0}}>
+                <div className="titlebar-nav">
+                    <h1>Books for You Library</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default TitleBar;
---
+++ src/components/app.jsx
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import TitleBar from './TitleBar/titleBar';
+import BookViewer from './BookViewer/bookViewer';
+import BookCreator from './BookCreator/bookCreator';
+import Footer from './Footer/footer';
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.books = [
+            {title: 'Ready Player One', author: 'Ernest Cline'},
+            {title: 'All the Light We Cannot See', author: 'Anthony Doerr'},
+            {title: 'The First and Last Freedom', author: 'Jiddu Krishnamurti'},
+
+        ];
+        this.state = {
+            bookNumber: 0 //Our Stateful variable 
+        }
+    }
+    addNewBook(book){
+        this.books.push(book);
+        this.setState({
+            bookNumber: this.books.length -1
+        });
+    }
+    goToNextBook(){
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber++;
+        if(tempBookNumber === this.books.length){
+            tempBookNumber = 0;
+        }
+        this.setState({
+            bookNumber : tempBookNumber
+        });
+    }
+    goToPreviousBook(){
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber--;
+        if(tempBookNumber < 0)
+        tempBookNumber = this.books.length -1;
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+    state = {  }
+    render() {
+        return (
+            <div className="container-fluid">
+                <TitleBar />
+                <BookViewer book={this.books[this.state.bookNumber]} nextBook={() => this.goToNextBook()} previousBook={() => this.goToPreviousBook()}/>
+                <BookCreator addNewBook={this.addNewBook.bind(this)}/>
+                <Footer />
+           </div> 
+        );
+    }
+}
+export default App;
+
+
+//This is our main/root component- We have created our class component also known as a "stateful component"
